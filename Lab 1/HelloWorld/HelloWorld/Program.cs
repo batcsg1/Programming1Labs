3ba02c40235bca37c108b4754ea3bb4b28059bce
Samuel Batchelor (1000125268) <BATCSG1@student.op.ac.nz>
2024-05-23 12:52:44 +1200

    Added lab 1

---
+++ Lab 1/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,24 @@
+﻿//This is traditionally the first program written.
+
+/*Sample Program to illustrate C# syntax
+ * Author: Samuel Batchelor
+ * Date 27 February 2024
+ */
+
+using System;
+
+namespace HelloWorld
+{
+    class Program 
+    {
+        
+       static void Main()
+        {
+            Console.WriteLine("Hello World");
+            Console.WriteLine("Welcome to IN510 Programming 1");
+            Console.WriteLine();
+            Console.ReadLine();
+         
+        }
+    }
+}
---
+++ Lab 1/StarsAndDetails/StarsAndDetails/Program.cs
@@ -0,0 +1,31 @@
+﻿/*****************************************************************
+ * Sample Program showing box of starts and demonstrating colors *
+ * Author: Samuel Batchelor                                      *
+ * Date 29 February 2024                                         *
+ *****************************************************************/
+
+
+namespace StarsAndDetails
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("***************************************");
+           
+            Console.WriteLine("* Samuel Batchelor                    *");
+            Console.WriteLine("* 149 Forfar Street                   *");
+            Console.WriteLine("* My favourite BIT Subject is: Maths  *");
+            Console.WriteLine("*                                     *");
+            Console.WriteLine("***************************************");
+
+            Console.WriteLine(2.3456 + 1);
+            Console.Write("what goes\nup \nmust come \tdown\n");
+            
+            Console.ReadLine();
+        }
+
+    }
+}
